74dde2f9c36f94fb8b7ad0b71f80a74d089c5f08
sami5671 <samialam5671@gmail.com>
2026-02-24 16:20:18 +0600

    Add GitHub Actions for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,13 +25,11 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
+    headless: true,
     baseURL: "http://automationexercise.com",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
+    trace: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
